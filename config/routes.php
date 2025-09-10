be54efe20aba1310ea371c1741264cb9e0259fca
Hugo Caixeta <hugoccaixeta3@gmail.com>
2025-09-10 09:40:35 -0300

    Criando controller das notas e ajustando rotas

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Route;
+use App\Controllers\Notas;
 use App\Controllers\IndexController;
 use App\Controllers\LoginController;
 use App\Controllers\DashboardController;
@@ -8,12 +9,15 @@ use App\Controllers\LogoutController;
 use App\Controllers\RegisterController;
 
 (new Route())
-
     ->get('/', IndexController::class)
+
     ->get('/login', [LoginController::class, 'login'])
     ->post('/login', [LoginController::class, 'login'])
 
     ->get('/dashboard', DashboardController::class)
+    ->get('/notas/criar', [Notas\CriarController::class, 'index'])
+    ->post('/notas/criar', [Notas\CriarController::class, 'store'])
+    
     ->get('/logout', LogoutController::class)
 
     ->get('/registrar', [RegisterController::class, 'index'])
--- views/notas/criar.view.php
+++ views/notas/criar.view.php
@@ -8,7 +8,7 @@
                 <li><a>Icone de olho</a></li>
                 <li>
                     <details>
-                        <summary><?= $user->nome ?></summary>
+                        <summary><?= auth()->nome ?></summary>
                         <ul class="bg-base-100 rounded-t-none p-2">
                             <li><a href="/logout">Logout</a></li>
                         </ul>
@@ -43,26 +43,25 @@
         </div>
 
         <!-- Form -->
-        <div class="bg-base-200 rounded-r-box w-full p-8 flex flex-col space-y-8">
+        <div class="bg-base-200 rounded-r-box w-full p-8">
 
-            <label class="form-control w-full">
-                <div class="label">
-                    <span class="label-text mb-2">Título</span>
+            <form action="/notas/criar" method="post" class="flex flex-col space-y-8">
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text mb-2">Título</span>
+                    </div>
+                    <input type="text" placeholder="Título da nota" class="input input-bordered w-full" />
+                </label>
+                <label class="form-control">
+                    <div class="label">
+                        <span class="label-text mb-2">Sua Nota</span>
+                    </div>
+                    <textarea placeholder="Descrição da nota" class="textarea h-26 textarea-bordered w-full"></textarea>
+                </label>
+                <div class="flex justify-end items-center mt-8">
+                    <button class="btn btn-primary">Salvar</button>
                 </div>
-                <input type="text" placeholder="Título da nota" class="input input-bordered w-full" />
-            </label>
-
-            <label class="form-control">
-                <div class="label">
-                    <span class="label-text mb-2">Sua Nota</span>
-                </div>
-                <textarea placeholder="Descrição da nota" class="textarea h-26 textarea-bordered w-full"></textarea>
-            </label>
-
-            <div class="flex justify-between items-center mt-8">
-                <button class="btn btn-secondary">Deletar</button>
-                <button class="btn btn-primary">Atualizar</button>
-            </div>
+            </form>
 
         </div>
     </div>
